c3e878606d2322dd96c67e590decfe07efb10933
Crowdbotics <team@crowdbotics.com>
2021-08-11 20:16:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "proweb_29729Identifier",
+  url: "https://proweb_29729Identifier.botics.co",
+  api: "https://proweb_29729Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
